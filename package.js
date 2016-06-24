921d960c3b3590d618fd47c91309fa2510bce2fe
steedos <zhuangjianguo@hotlong.com>
2016-06-24 11:41:17 +0800

    update package versions

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) { 
-    api.versionsFrom('1.0');
+    api.versionsFrom("1.2.1");
 
     api.use('reactive-var');
     api.use('reactive-dict');
@@ -16,31 +16,30 @@ Package.onUse(function(api) {
     api.use('check');
     api.use('ddp-rate-limiter');
     api.use('underscore');
-    api.use('underscorestring:underscore.string');
     api.use('tracker');
     api.use('session');
     api.use('blaze');
     api.use('templating');
+    api.use('webapp', 'server');
     
-    api.use('flemay:less-autoprefixer');
-    api.use('simple:json-routes');
-    api.use('nimble:restivus');
-    api.use('aldeed:simple-schema');
-    api.use('aldeed:collection2');
-    api.use('aldeed:tabular');
-    api.use('aldeed:autoform');
-    api.use('matb33:collection-hooks');
-    api.use('cfs:standard-packages');
-    api.use('kadira:blaze-layout');
-    api.use('kadira:flow-router');
+    api.use('flemay:less-autoprefixer@1.2.0');
+    api.use('simple:json-routes@2.1.0');
+    api.use('nimble:restivus@0.8.7');
+    api.use('aldeed:simple-schema@1.3.3');
+    api.use('aldeed:collection2@2.5.0');
+    api.use('aldeed:tabular@1.6.0');
+    api.use('aldeed:autoform@5.8.0');
+    api.use('matb33:collection-hooks@0.8.1');
+    api.use('cfs:standard-packages@0.5.9');
+    api.use('kadira:blaze-layout@2.3.0');
+    api.use('kadira:flow-router@2.10.1');
 
-    api.use('meteorhacks:ssr@2.2.0', 'server');
-    api.use('steedos:lib');
+    api.use('meteorhacks:ssr@2.2.0');
+    api.use('steedos:lib@0.0.1');
+    api.use('tap:i18n@1.7.0');
 
-    api.use(['webapp'], 'server');
 
 
-    api.use('tap:i18n', ['client', 'server']);
     //api.add_files("package-tap.i18n", ["client", "server"]);
     tapi18nFiles = ['i18n/en.i18n.json', 'i18n/zh-CN.i18n.json']
     api.addFiles(tapi18nFiles, ['client', 'server']);
